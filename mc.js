5bfdd69db93894eb0fc9075faff69f35aec0140a
Muhammet Caglar <1muhammetcaglar@gmail.com>
2023-06-16 17:45:59 +0300

    Real time validation added

--- mc.js
+++ mc.js
@@ -1,23 +1,31 @@
 const form=document.querySelector('.signupForm');
 const message=document.querySelector('.message');
-
+const pattern=/^[a-zA-Z]{6,10}$/;
 
 
 form.addEventListener('submit',e =>{
     e.preventDefault();
-    const pattern=/^[a-zA-Z]{6,10}$/;
+    
     const username=form.username.value;
     if(pattern.test(username)){
         message.textContent='Format Doğru';
-        form.setAttribute('style','border:3px solid green');
+        
     }
     else{
         message.textContent='Format yanlış, sadece harf giriniz ve 6-10 karakter arasında olsun.';
-        form.setAttribute('style','border:3px solid red');
+    
     }
     //sayfanın refresh olmamasını sağlar
     // console.log('form gönderildi');
     // console.log(username.value);
-   
 
+})
+
+form.username.addEventListener('keyup',e=>{
+    if(pattern.test(e.target.value)){
+      form.username.setAttribute('class','success');
+    }
+    else{
+        form.username.setAttribute('class','error');
+    }
 })
